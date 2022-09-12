27f0f3c9e061fea48ad463384bfa293fe42aa288
Luis Blanco <ljblanco@redborder.com>
2022-09-12 12:13:03 +0100

    b is bad for bash. Debug as simple arg. enrich def

--- resources/bin/rb_scan_vulnerabilities.sh
+++ resources/bin/rb_scan_vulnerabilities.sh
@@ -7,22 +7,24 @@ PORTS="all"
 SCAN_ID=""
 BATCH_RATE="0.1"
 KAFKA="127.0.0.1:9092"
+ENRICH="{}"
 
 function usage() {
-  echo "$0 [-t <target> -p <ports> -s <scan id> -e <enrichment> -b <batch rate> -k <kafka address>][-h]"
+  echo "$0 [-t <target> -p <ports> -s <scan id> -e <enrichment> -br <batch rate> -k <kafka address>][-h(help) -d(debug)]"
 }
 
 IFS=$'\n\t'
 
-while getopts "t:p:s:e:b:k:h" name; do
+while getopts "t:p:s:e:br:k:hd" name; do
   case $name in
     t) TARGET=$OPTARG ;;
     p) PORTS=$OPTARG ;;
     s) SCAN_ID=$OPTARG ;;
     e) ENRICH=$OPTARG ;;
-    b) BATCH_RATE=$OPTARG ;;
+    br) BATCH_RATE=$OPTARG ;;
     k) KAFKA=$OPTARG ;;
     h) usage ;;
+    d) DEBUG="-d" ;;
   esac
 done
 
@@ -40,11 +42,6 @@ if [ "x$SCAN_ID" == "x" ]; then
     exit
 fi
 
-if [ "x$PORTS" == "x" ]; then
-    ENRICH="{}"
-fi
-
-
 SCRIPT_RUBY_PATH="/opt/rb/bin/rb_scan_vulnerabilities.rb"
 
 # If we are in redborder-ng ..
@@ -52,4 +49,4 @@ if [[ -f "/usr/lib/redborder/scripts/rb_scan_vulnerabilities.rb" ]]; then
   SCRIPT_RUBY_PATH="/usr/lib/redborder/scripts/rb_scan_vulnerabilities.rb"
 fi
 
-ruby $SCRIPT_RUBY_PATH -t $TARGET -p $PORTS -s $SCAN_ID -e $ENRICH -b $BATCH_RATE -k $KAFKA
+ruby $SCRIPT_RUBY_PATH -t $TARGET -p $PORTS -s $SCAN_ID -e $ENRICH -b $BATCH_RATE -k $KAFKA $DEBUG
